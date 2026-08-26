7a13f5046614ee2c0832ed33ce0587099015629e
DungNgMechRev <nguyendinhvuongdung@gmail.com>
2026-08-26 10:46:05 +0800

    leet56

---
+++ L2/leet56.cpp
@@ -0,0 +1,32 @@
+class Solution
+{
+public:
+    vector<vector<int>> merge(vector<vector<int>> &intervals)
+    {
+        vector<vector<int>> res;
+
+        sort(intervals.begin(), intervals.end());
+        int currentStart = intervals[0][0];
+        int currentEnd = intervals[0][1];
+        int n = intervals.size();
+        int i = 1;
+        for (; i < n; i++)
+        {
+            // if start before currend -> merge
+            if (intervals[i][0] <= currentEnd)
+            {
+                // ko can update current start vi khi sort no tu sort theo thang dau tien -> luon start sau curr start
+                currentEnd = max(currentEnd, intervals[i][1]);
+            }
+            else
+            {
+                res.push_back({currentStart, currentEnd});
+                currentStart = intervals[i][0];
+                currentEnd = intervals[i][1];
+            }
+        }
+
+        res.push_back({currentStart, currentEnd});
+        return res;
+    }
+};
